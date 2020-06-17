6ecb5e5df199f8abeba4bfff186b9ad3d89e7010
Alex George <alexjgeorge636@gmail.com>
2020-06-17 17:36:45 -0400

    Doubles are successfully converted to byte array and sent across in UDP packet.

--- src/generator.c
+++ src/generator.c
@@ -9,36 +9,53 @@
 #define LOCAL_IP "127.0.0.1"
 #define LOCAL_PORT 5367
 
-#define BUFF_SIZE 4
-
-double buffer[BUFF_SIZE];
+#define BUFF_SIZE 32
 
 // Function converting degrees to radians.
 double to_radians(double degrees) {
     return M_PI*(degrees/180.0);
 }
 
-// Function generating three phase outputs as array.
-double * threephase(double degrees) {
+// Function convert a double data type into a byte array
+void double2byte(unsigned char temp_bytes[8], double value) {
+    union {
+        double num;
+        unsigned char bytes[8];
+    } unionized;
+
+    unionized.num = value;
+    memcpy(temp_bytes, unionized.bytes, 8);
+}
+
+// Function will concatenate 4x 8 byte char arrays.
+static unsigned char concatenate(unsigned char arr1[], unsigned char arr2[], unsigned char arr3[], unsigned char arr4[], unsigned char conarray[]) {
+    memcpy(conarray + 0, arr1, sizeof(arr1));
+    memcpy(conarray + 8, arr2, sizeof(arr2));
+    memcpy(conarray + 16, arr3, sizeof(arr3));
+    memcpy(conarray + 24, arr4, sizeof(arr4));
+
+    return conarray;
+}
+
+// Function generating three phase outputs as array and forms UDP payload.
+static unsigned char * threephase(double degrees) {
     double p1 = to_radians(degrees);
     double p2 = to_radians(degrees + 120.0);
     double p3 = to_radians(degrees + 240.0);
 
     // C does not return the address of a local var outside function, therefore use static variable.
-    static double array[3];
-    array[0] = 120*sin(p1);
-    array[1] = 120*sin(p2);
-    array[2] = 120*sin(p3);
-    return array;
-}
+    static unsigned char payload[32];
+    unsigned char degree_arr[8];
+    unsigned char phase_1[8];
+    unsigned char phase_2[8];
+    unsigned char phase_3[8];
+    double2byte(degree_arr, degrees);
+    double2byte(phase_1, sin(p1));
+    double2byte(phase_2, sin(p2));
+    double2byte(phase_3, sin(p3));
 
-// Function generating UDP payload
-double UDP_payload(double degrees) {
-    double *threephase_arr = threephase(degrees);
-    buffer[0] = degrees;
-    buffer[1] = *(threephase_arr);
-    buffer[2] = *(threephase_arr + 1);
-    buffer[3] = *(threephase_arr + 2);
+    concatenate(degree_arr, phase_1, phase_2, phase_3, payload);
+    return payload;
 }
 
 int main(void) {
@@ -58,17 +75,32 @@ int main(void) {
     clientaddr.sin_addr.s_addr = inet_addr(LOCAL_IP);
 
     while (1) {
-        sendto(sockfd, buffer, BUFF_SIZE, 0, (const struct sockaddr *)&clientaddr, sizeof(clientaddr));
+        unsigned char *threephase_arr = threephase(deg);
+        int status = sendto(sockfd, threephase_arr, BUFF_SIZE, 0, (const struct sockaddr *)&clientaddr, sizeof(clientaddr));
         // Label which degree/radian currently executing.
         double rad = to_radians(deg);
+        printf("Send Status: %d\n", status);
         printf("Degrees: %f -- Radians: %f\n", deg, rad);
 
-        double *threephase_arr = threephase(deg);
-        for (int i=0; i<3; i++) {
-            printf("Phase %d: %f\n", i+1, *(threephase_arr+i));
+        int i;
+        printf("\nDegree is:");
+        for (i=0;i<8;i++) {
+            printf ("%02x", threephase_arr[i]);
         }
-        printf("\n");
+        printf("\nPhase 1 is:");
+        for (i=8;i<16;i++) {
+            printf ("%02x", threephase_arr[i]);
+        }
+        printf("\nPhase 2 is:");
+        for (i=16;i<24;i++) {
+            printf ("%02x", threephase_arr[i]);
+        }
+        printf("\nPhase 3 is:");
+        for (i=24;i<32;i++) {
+            printf ("%02x", threephase_arr[i]);
+        }
+        printf("\n\n");
         deg += 1.0;
-        usleep(5000);
+        usleep(500000);
     }
 }
